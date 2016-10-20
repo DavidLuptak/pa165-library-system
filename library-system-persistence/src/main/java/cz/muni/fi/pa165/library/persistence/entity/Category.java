191e4cfc0a59f97cc6eaa25b612c76cfd3c8d54c
Dávid Lupták <luptak.david@gmail.com>
2016-10-20 23:23:53 +0200

    [ADD] Category entity

---
+++ library-system-persistence/src/main/java/cz/muni/fi/pa165/library/persistence/entity/Category.java
@@ -0,0 +1,80 @@
+package cz.muni.fi.pa165.library.persistence.entity;
+
+import javax.persistence.*;
+import javax.swing.text.StringContent;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author David Luptak
+ * @version 20.10.2016
+ */
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(nullable = false, unique = true)
+    private String name;
+
+//    @ManyToMany
+//    private List<Book> books = new ArrayList<Book>();
+
+
+    public Category() {
+    }
+
+    public Category(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+//    public void addBook(Book book) {
+//        this.books.add(book);
+//    }
+//
+//    public List<Book> getBooks() {
+//        return Collections.unmodifiableList(books);
+//    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        if (!(o instanceof Category)) return false;
+
+        Category category = (Category) o;
+
+        if (!id.equals(category.id)) return false;
+        return name.equals(category.name);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + name.hashCode();
+        return result;
+    }
+}
