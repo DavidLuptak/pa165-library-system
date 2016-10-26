e0410a2194d560978e854e8d0ef415226acf4f74
LenkaJaneckova <lenka.janeckova88@gmail.com>
2016-10-26 23:55:15 +0200

    Created BookCopyDao and BookCopyDaoImpl

---
+++ library-system-persistence/src/main/java/cz/muni/fi/pa165/library/dao/BookCopyDao.java
@@ -0,0 +1,45 @@
+package cz.muni.fi.pa165.library.dao;
+
+import java.util.List;
+
+import cz.muni.fi.pa165.library.entity.Book;
+import cz.muni.fi.pa165.library.entity.BookCopy;
+
+/**
+ * @author Lenka (433591)
+ * @version 26.10.2016
+ */
+public interface BookCopyDao {
+
+    /**
+     * Saves the given bookCopy into the database
+     * @param bookCopy to be saved
+     */
+    void create(BookCopy bookCopy);
+
+    /**
+     * Updates the given bookCopy in the database
+     * @param bookCopy to be updated
+     */
+    void update(BookCopy bookCopy);
+
+    /**
+     * Deletes the given bookCopy from the database
+     * @param bookCopy to be deleted
+     */
+    void delete(BookCopy bookCopy);
+
+    /**
+     * Finds a bookCopy with the given id in the database
+     * @param id of searched bookCopy
+     * @return found bookCopy
+     */
+    BookCopy findById(long id);
+
+    /**
+     * Finds all copies of the given book in the database
+     * @param book searched book
+     * @return found copies
+     */
+    List<BookCopy> findByBook(Book book);
+}
---
+++ library-system-persistence/src/main/java/cz/muni/fi/pa165/library/dao/BookCopyDaoImpl.java
@@ -0,0 +1,60 @@
+package cz.muni.fi.pa165.library.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import cz.muni.fi.pa165.library.entity.Book;
+import cz.muni.fi.pa165.library.entity.BookCopy;
+import org.apache.commons.lang.Validate;
+
+/**
+ * @author Lenka (433591)
+ * @version 26.10.2016
+ */
+//@Reposittory
+public class BookCopyDaoImpl implements BookCopyDao {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public void create(BookCopy bookCopy) {
+        validateBookCopy(bookCopy);
+        Validate.isTrue(bookCopy.getId() == null, "bookCopy's id should be null");
+        em.persist(bookCopy);
+    }
+
+    @Override
+    public void update(BookCopy bookCopy) {
+        validateBookCopy(bookCopy);
+        Validate.notNull(bookCopy.getId());
+        em.merge(bookCopy);
+    }
+
+    @Override
+    public void delete(BookCopy bookCopy) {
+        Validate.notNull(bookCopy.getId());
+        em.remove(bookCopy);
+    }
+
+    @Override
+    public BookCopy findById(long id) {
+        return em.find(BookCopy.class, id);
+    }
+
+    @Override
+    public List<BookCopy> findByBook(Book book) {
+        Validate.notNull(book);
+        return em.createQuery("SELECT b FROM BookCopy b where b.book = :book", BookCopy.class)
+            .setParameter("book", book).getResultList();
+
+    }
+
+    private void validateBookCopy(BookCopy bookCopy) {
+        Validate.notNull(bookCopy);
+        Validate.notNull(bookCopy.getBook());
+        Validate.notNull(bookCopy.getBookState());
+    }
+}
