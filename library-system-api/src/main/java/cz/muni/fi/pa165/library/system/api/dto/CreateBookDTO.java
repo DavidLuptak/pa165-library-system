4a68e23a1bd4649dd44fc8194c8ee1f578d1263e
Bedrich Said <bedrich.said@gmail.com>
2016-11-09 18:00:59 +0100

    Added BookFacade into API.

---
+++ library-system-api/src/main/java/cz/muni/fi/pa165/library/system/api/dto/CreateBookDTO.java
@@ -0,0 +1,9 @@
+package cz.muni.fi.pa165.library.system.api.dto;
+
+/**
+ *
+ * @author Bedrich Said
+ */
+public class CreateBookDTO {
+    
+}
---
+++ library-system-api/src/main/java/cz/muni/fi/pa165/library/system/api/facade/BookFacade.java
@@ -0,0 +1,60 @@
+package cz.muni.fi.pa165.library.system.api.facade;
+
+import cz.muni.fi.pa165.library.system.api.dto.BookDTO;
+import cz.muni.fi.pa165.library.system.api.dto.CreateBookDTO;
+import cz.muni.fi.pa165.library.enums.BookState;
+import java.util.List;
+
+/**
+ * Facade layer for Book entity
+ * 
+ * @author Bedrich Said
+ */
+public interface BookFacade {
+    /**
+     * Persists book into database
+     *
+     * @param book book to be persisted
+     * @return new book id
+     */
+    Long createBook(CreateBookDTO book);
+
+    /**
+     * Returns book with given id or null
+     *
+     * @param id book id
+     * @return book or null
+     */
+    BookDTO findById(Long id);
+
+    /**
+     * Finds books by name
+     *
+     * @param name book name
+     * @return list of discovered books
+     */
+    List<BookDTO> findByName(String name);
+
+    /**
+     * Get all persisted books
+     *
+     * @return list of all books
+     */
+    List<BookDTO> findAll();
+
+    /**
+     * Removes book from the database
+     *
+     * @param bookId book id
+     */
+    void delete(Long bookId);
+
+    /**
+     * Sets book current state. Can be set only to a worse (or the same) state
+     * than it already was
+     *
+     * @param bookId book id
+     * @param newState new state to be set
+     */
+    void setState(Long bookId, BookState newState);
+}
