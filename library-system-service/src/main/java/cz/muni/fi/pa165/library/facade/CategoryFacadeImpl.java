4b5cb366f30684851c4acbd2f24cb2a83f79ceb6
Dávid Lupták <luptak.david@gmail.com>
2016-11-25 01:37:30 +0100

    Category facade tests correction and extension.

--- library-system-service/src/main/java/cz/muni/fi/pa165/library/facade/CategoryFacadeImpl.java
+++ library-system-service/src/main/java/cz/muni/fi/pa165/library/facade/CategoryFacadeImpl.java
@@ -3,6 +3,7 @@ package cz.muni.fi.pa165.library.facade;
 import cz.muni.fi.pa165.library.dto.CategoryDTO;
 import cz.muni.fi.pa165.library.dto.CategoryNewDTO;
 import cz.muni.fi.pa165.library.entity.Category;
+import cz.muni.fi.pa165.library.exception.NoEntityFoundException;
 import cz.muni.fi.pa165.library.mapping.BeanMappingService;
 import cz.muni.fi.pa165.library.service.CategoryService;
 import org.springframework.stereotype.Service;
@@ -27,6 +28,10 @@ public class CategoryFacadeImpl implements CategoryFacade {
 
     @Override
     public Long create(CategoryNewDTO categoryNewDTO) {
+        if (categoryNewDTO == null) {
+            throw new IllegalArgumentException("Category cannot be null.");
+        }
+
         Category category = beanMappingService.mapTo(categoryNewDTO, Category.class);
         categoryService.create(category);
         return category.getId();
@@ -34,23 +39,62 @@ public class CategoryFacadeImpl implements CategoryFacade {
 
     @Override
     public void update(CategoryDTO categoryDTO) {
+        if (categoryDTO == null) {
+            throw new IllegalArgumentException("Category cannot be null.");
+        }
+
         Category category = beanMappingService.mapTo(categoryDTO, Category.class);
+
+        if (categoryService.findById(category.getId()) == null) {
+            throw new NoEntityFoundException("Category not found during update.");
+        }
+
         categoryService.update(category);
     }
 
     @Override
     public void delete(Long id) {
-        categoryService.delete(categoryService.findById(id));
+        if (id == null) {
+            throw new IllegalArgumentException("Category id cannot be null.");
+        }
+
+        Category category = categoryService.findById(id);
+
+        if (category == null) {
+            throw new NoEntityFoundException("Category not found during delete.");
+        }
+
+        categoryService.delete(category);
     }
 
     @Override
     public CategoryDTO findById(Long id) {
-        return beanMappingService.mapTo(categoryService.findById(id), CategoryDTO.class);
+        if (id == null) {
+            throw new IllegalArgumentException("Category id cannot be null.");
+        }
+
+        Category category = categoryService.findById(id);
+
+        if (category == null) {
+            throw new NoEntityFoundException("Category not found during findById.");
+        }
+
+        return beanMappingService.mapTo(category, CategoryDTO.class);
     }
 
     @Override
     public CategoryDTO findByName(String name) {
-        return beanMappingService.mapTo(categoryService.findByName(name), CategoryDTO.class);
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be null.");
+        }
+
+        Category category = categoryService.findByName(name);
+
+        if (category == null) {
+            throw new NoEntityFoundException("Category not found during findByName.");
+        }
+
+        return beanMappingService.mapTo(category, CategoryDTO.class);
     }
 
     @Override
--- library-system-service/src/test/java/cz/muni/fi/pa165/library/facade/CategoryFacadeTest.java
+++ library-system-service/src/test/java/cz/muni/fi/pa165/library/facade/CategoryFacadeTest.java
@@ -3,25 +3,20 @@ package cz.muni.fi.pa165.library.facade;
 import cz.muni.fi.pa165.library.config.ServiceConfiguration;
 import cz.muni.fi.pa165.library.dto.CategoryDTO;
 import cz.muni.fi.pa165.library.dto.CategoryNewDTO;
-import cz.muni.fi.pa165.library.dto.CategoryDTO;
-import cz.muni.fi.pa165.library.dto.CategoryNewDTO;
 import cz.muni.fi.pa165.library.entity.Book;
 import cz.muni.fi.pa165.library.entity.Category;
-import cz.muni.fi.pa165.library.entity.Category;
+import cz.muni.fi.pa165.library.exception.NoEntityFoundException;
 import cz.muni.fi.pa165.library.mapping.BeanMappingService;
 import cz.muni.fi.pa165.library.mapping.BeanMappingServiceImpl;
-import cz.muni.fi.pa165.library.service.BookService;
 import cz.muni.fi.pa165.library.service.CategoryService;
 import org.mockito.*;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import javax.inject.Inject;
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -69,19 +64,21 @@ public class CategoryFacadeTest extends AbstractTransactionalTestNGSpringContext
         book1 = new Book("bookName1", "bookAuthor1", "1111111111");
         book1.setId(1L);
         book2 = new Book("bookName2", "bookAuthor2", "2222222222");
-        book1.setId(2L);
+        book2.setId(2L);
         book3 = new Book("bookName3", "bookAuthor3", "3333333333");
-        book1.setId(3L);
+        book3.setId(3L);
         category1 = new Category("categoryName1");
         category1.setId(1L);
         category2 = new Category("categoryName2");
-        category1.setId(2L);
+        category2.setId(2L);
     }
 
     @BeforeMethod(dependsOnMethods = "initEntities")
     public void initMockBehaviour() {
         when(categoryService.findById(1L)).thenReturn(category1);
+        when(categoryService.findById(0L)).thenReturn(null);
         when(categoryService.findByName("categoryName1")).thenReturn(category1);
+        when(categoryService.findByName("noCategory")).thenReturn(null);
         when(categoryService.findAll()).thenReturn(Arrays.asList(category1, category2));
     }
 
@@ -98,6 +95,11 @@ public class CategoryFacadeTest extends AbstractTransactionalTestNGSpringContext
         assertEquals(category1.getName(), entity.getName());
     }
 
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testCreateNull() {
+        categoryFacade.create(null);
+    }
+
     @Test
     public void testUpdate() {
         categoryDTO = new CategoryDTO(category2.getName());
@@ -106,13 +108,36 @@ public class CategoryFacadeTest extends AbstractTransactionalTestNGSpringContext
         categoryFacade.update(categoryDTO);
 
         verify(categoryService).update(categoryArgumentCaptor.capture());
-        assertEquals(categoryDTO.getName(),category2.getName());
+        assertEquals(categoryDTO.getName(), category2.getName());
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testUpdateNull() {
+        categoryFacade.update(null);
+    }
+
+    @Test(expectedExceptions = NoEntityFoundException.class)
+    public void testUpdateNonExisting() {
+        categoryDTO = new CategoryDTO("cat");
+        categoryDTO.setId(0L);
+
+        categoryFacade.update(categoryDTO);
     }
 
     @Test
     public void testDelete() {
         categoryFacade.delete(1L);
         verify(categoryService).delete(category1);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testDeleteNull() {
+        categoryFacade.delete(null);
+    }
+
+    @Test(expectedExceptions = NoEntityFoundException.class)
+    public void testDeleteNonExisting() {
+        categoryFacade.delete(0L);
     }
 
     @Test
@@ -122,11 +147,31 @@ public class CategoryFacadeTest extends AbstractTransactionalTestNGSpringContext
         assertEqualsCategoryAndCategoryDTO(category1, categoryDTO);
     }
 
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testFindByIdNull() {
+        categoryDTO = categoryFacade.findById(null);
+    }
+
+    @Test(expectedExceptions = NoEntityFoundException.class)
+    public void testFindByIdNonExisting() {
+        categoryDTO = categoryFacade.findById(0L);
+    }
+
     @Test
     public void testFindByName() {
         categoryDTO = categoryFacade.findByName(category1.getName());
         assertNotNull(categoryDTO);
         assertEqualsCategoryAndCategoryDTO(category1, categoryDTO);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testFindByNameNull() {
+        categoryDTO = categoryFacade.findByName(null);
+    }
+
+    @Test(expectedExceptions = NoEntityFoundException.class)
+    public void testFindByNameNonExisting() {
+        categoryDTO = categoryFacade.findByName("noCategory");
     }
 
     @Test
