bcb02f1ff363bc17ef1f1365f7b23b58c347c8ed
Bedrich Said <bedrich.said@gmail.com>
2016-12-16 09:59:38 +0100

    Implemented UserDetailsAdapter

--- library-system-web/src/main/java/cz/muni/fi/pa165/library/web/security/UserDetailsAdapter.java
+++ library-system-web/src/main/java/cz/muni/fi/pa165/library/web/security/UserDetailsAdapter.java
@@ -1,16 +1,71 @@
 package cz.muni.fi.pa165.library.web.security;
 
 import cz.muni.fi.pa165.library.dto.UserDTO;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 /**
  *
  * @author Bedrich Said
  */
-public class UserDetailsAdapter {
+public class UserDetailsAdapter implements UserDetails {
+    //private static final long serialVersionUID = 1L;
+    private UserDTO dto;
+    
+    public UserDetailsAdapter(UserDTO dto) {
+        this.dto = dto;
+    }
 
     public UserDTO getDto() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return dto;
     }
-    
-    //todo
+
+    public void setDto(UserDTO dto) {
+        this.dto = dto;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+        if (dto.isAdmin()) {
+            authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        }
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return dto.getPasswordHash();
+    }
+
+    @Override
+    public String getUsername() {
+        //todo: are we using email as username?
+        return dto.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
